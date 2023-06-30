9f8afe2827cb0e556fbc23cc9373159d46b248e7
yousef-romany <yousefromany527@gmail.com>
2023-06-30 04:22:23 +0200

    push

--- routes/notification/notification.js
+++ routes/notification/notification.js
@@ -1,3 +1,12 @@
 module.exports = function (app, connection) {
-
+    app.get("/notificationAlert", (req, res) => {
+        let sql = "SELECT * FROM rawmaterials right JOIN storagerawmatrail ON rawmaterials.ID = storagerawmatrail.IDMATRAIL;";
+        connection.query(sql , (error, result) => {
+            if(error) {
+                console.log(error)
+                res.send("error in database");
+            }
+            res.json(result);
+        });
+    });
 };
